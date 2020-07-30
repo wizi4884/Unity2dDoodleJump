0237ade7eaca93cc5d0673bb860d2c0209b38f5c
wizi4884 <68853051+wizi4884@users.noreply.github.com>
2020-07-30 16:00:02 +0300

    Camera

---
+++ CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform doodlesPos; //Позиция дудлика
+
+    void Update()
+    {
+        if (doodlesPos.position.y > transform.position.y)// если позиция дудлика больше позициии камеры
+        {
+            // то позиция камеры приравнивается к позиции дудлика
+            transform.position = new Vector3(transform.position.x, doodlesPos.position.y, transform.position.z);
+        }
+        
+    }
+}
