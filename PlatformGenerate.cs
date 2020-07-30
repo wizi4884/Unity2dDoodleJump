50378dd8f177d8e1bb2680e05935f19be78b3fae
wizi4884 <68853051+wizi4884@users.noreply.github.com>
2020-07-30 15:59:30 +0300

    PlatformGenerator

---
+++ PlatformGenerate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerate : MonoBehaviour
+{
+    public GameObject platformPrefab;//Переменная для префаба
+   
+    void Start()
+    {
+        Vector3 SpawnerPosition = new Vector3(); //Создаем новый вектор
+
+        for (int i = 0; i < 10; i++)//проходим 10 раз
+        {
+            SpawnerPosition.x = Random.Range(-1.7f, 1.7f);//по х
+            SpawnerPosition.y += Random.Range(2f, 4f);//по у
+
+            Instantiate(platformPrefab, SpawnerPosition, Quaternion.identity);//Создаем новые платформы
+        }
+
+    }
+
+}
